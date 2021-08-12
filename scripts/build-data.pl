d896d51feb2795088bf98a24eedfda41458dc7ab
drbaggy <js5@sanger.ac.uk>
2021-08-12 06:47:41 +0100

    pushing changes after tidying up - and missing pages

--- scripts/build-data.pl
+++ scripts/build-data.pl
@@ -25,22 +25,23 @@ my %host_map = rev_hash( qw(mosquito human mouse)  );
 
 foreach my $key (@ARGV) {
   my $struct = LoadFile( $root.'/configs/'.$key.'.yaml' );
-  mkdir $doc_root.'processed'       unless -d 'processed';
-  mkdir $doc_root.'processed/'.$key unless -d 'processed/'.$key;
+## Make directories...
+  my $p_dir = $doc_root.'processed'; mkdir $p_dir  unless -d $p_dir; $p_dir.='/'.$key; mkdir $p_dir  unless -d $p_dir;
+  my $d_dir = $root.'/dumps';        mkdir $d_dir  unless -d $d_dir; $d_dir.='/'.$key; mkdir $d_dir  unless -d $d_dir;
+## Parse data, mol & knn files...
   foreach my $md ( qw(ss2 ch10x) ) {
-    next unless -e "$source$key/$md/data.csv";
-    mkdir $doc_root.'processed/'.$key.'/'.$md        unless -d 'processed/'.$key.'/'.$md;
-    mkdir $doc_root.'processed/'.$key.'/'.$md.'/exp' unless -d 'processed/'.$key.'/'.$md.'/exp';
-    parse( "$source$key/$md/data.csv", $struct->{$md}{'cell'} );
-    $struct->{$md}{'cell'}{'genes'} = parse_mol( "$source$key/$md/exp.csv",$doc_root.'processed/'.$key.'/'.$md.'/exp' );
+    my $s_dir = $source.$key.'/'.$md;
+    parse_knn( $s_dir.'/knn.csv', $struct->{$md}{'gene'} ) if -e $s_dir.'/knn.csv';
+    next unless -e $s_dir.'/data.csv';
+    mkdir $p_dir.'/'.$md        unless -d $p_dir.'/'.$md;
+    mkdir $p_dir.'/'.$md.'/exp' unless -d $p_dir.'/'.$md.'/exp';
+    parse( $s_dir.'/data.csv', $struct->{$md}{'cell'} );
+    $struct->{$md}{'cell'}{'genes'} = parse_mol( $s_dir.'/exp.csv', $p_dir.'/'.$md.'/exp' );
   }
-  foreach my $md (qw(ss2 10x)) {
-    next unless -e "$source$key/$md/knn.csv";
-    parse_knn( "$source$key/$md/knn.csv", $struct->{$md}{'gene'} )        if -e "input/$key/$md/knn.csv";
-  }
-  open my $fh, '>', $doc_root."processed/$key/data.json"; print {$fh} encode_json($struct);close$fh;
-  #open $fh, '>', $doc_root."processed/$key/data.perl"; print {$fh} Dumper($struct);     close $fh;
-  #open $fh, '>', $doc_root."processed/$key/data.yaml"; print {$fh} Dump($struct);       close $fh;
+## Dump json file (and yaml/perl?)
+  open my $fh, '>', $p_dir.'/data.json'; print {$fh} encode_json($struct); close $fh;
+ #open    $fh, '>', $d_dir.'/data.perl'; print {$fh} Dumper($struct);      close $fh;
+  open    $fh, '>', $d_dir.'/data.yaml'; print {$fh} Dump($struct);        close $fh;
 }
 
 sub rev_hash { my $c = 0; return map { $_ => $c++ } @_; }
@@ -60,7 +61,7 @@ sub parse_mol {
     $n=~s{"}{}g;
     $n=~s{-}{_}g;
     push @genes,$n;
-    open my $ofh, q(>), "$key/$n.json";
+    open my $ofh, q(>), $key.'/'.$n.'.json';
     say {$ofh} '{"max":',$m,',"data":[',(join q(,),@V), ']}';
     close $ofh;
   }
@@ -80,7 +81,7 @@ sub parse_knn {
   my $x_col = $key_map{ 'KNN_GRAPH_X' };
   my $y_col = $key_map{ 'KNN_GRAPH_Y' };
   my $c_col = $key_map{ 'CLUSTER' };
-  my @indexes = map { $key_map{ $_ } } grep { ! /(KNN_GRAPH_|CLUSTER)/ && $_ ne 'GO BP'} @KEYS;
+  my @indexes = map { $key_map{ $_ } } grep { ! /(KNN_GRAPH_|CLUSTER)/ && $_ ne 'GO.BP'} @KEYS;
   my %clusters;
   my($x0,$y0,$x1,$y1);
   foreach( @{$data} ) {
@@ -112,18 +113,20 @@ sub parse {
   chomp $header;
   my %cols;
   my @data;
-  no warnings qw(uninitialized);
+  no warnings qw(uninitialized); ## Far too many uninitialized warnings caused by nulls in data!
   while(<$fh>) {
     chomp;
     s/\s+$//;
     my @T = map { $_ eq 'NA' ? undef : $_ } split /,/;
     my ($id,$p1,$p2,$p3,$u1,$u2,$u3,$cl,$st_lr,$st_hr,$str,$day,$host) = @T;
+    ## Truncate x/y/z to 5 decimal places...
     push @{$cols{'p1'}}, 0 + sprintf '%0.5f', $p1;
     push @{$cols{'p2'}}, 0 + sprintf '%0.5f', $p2;
     push @{$cols{'p3'}}, 0 + sprintf '%0.5f', $p3;
     push @{$cols{'u1'}}, 0 + sprintf '%0.5f', $u1;
     push @{$cols{'u2'}}, 0 + sprintf '%0.5f', $u2;
     push @{$cols{'u3'}}, 0 + sprintf '%0.5f', $u3;
+    ## Get the ranges...
     $r{'p1'}[0] = $p1 unless !defined $p1 || exists $r{'p1'}[0] && $r{'p1'}[0] < $p1;
     $r{'p2'}[0] = $p2 unless !defined $p2 || exists $r{'p2'}[0] && $r{'p2'}[0] < $p2;
     $r{'p3'}[0] = $p3 unless !defined $p3 || exists $r{'p3'}[0] && $r{'p3'}[0] < $p3;
@@ -136,22 +139,26 @@ sub parse {
     $r{'u1'}[1] = $u1 unless !defined $u1 || exists $r{'u1'}[1] && $r{'u1'}[1] > $u1;
     $r{'u2'}[1] = $u2 unless !defined $u2 || exists $r{'u2'}[1] && $r{'u2'}[1] > $u2;
     $r{'u3'}[1] = $u3 unless !defined $u3 || exists $r{'u3'}[1] && $r{'u3'}[1] > $u3;
-    $v{'st_lr' }{$st_lr}++     if defined $st_lr;
-    $v{'stage' }{$stage_map{$st_hr}}++     if defined $st_hr;
-    $v{'str'   }{$str}++       if defined $str;
+    ## Keep a track of all column values seen...
+    $v{'st_lr' }{$st_lr}++               if defined $st_lr;
+    $v{'stage' }{$stage_map{$st_hr}}++   if defined $st_hr;
+    $v{'str'   }{$str}++                 if defined $str;
     $v{'day'   }{$day_map{$day}}++       if defined $day;
-    $v{'host'  }{$host_map{$host}}++      if defined $host;
+    $v{'host'  }{$host_map{$host}}++     if defined $host;
     $v{'st'}{$st_lr}{$st_hr}++ if defined $st_lr;
     my @row=();
+    ## Create custom data structure....
     foreach(@{$res->{'columns'}}) {
-      push @row, $stage_map{$st_hr} if $_ eq 'stage';
+      push @row, $stage_map{$st_hr}                                if $_ eq 'stage';
       push @row, exists $day_map{$day} ? $day_map{  $day} : $day   if $_ eq 'day';
-      push @row, $host_map{$host}   if $_ eq 'host';
+      push @row, $host_map{$host}                                  if $_ eq 'host';
     };
     push @data, \@row;
   }
   use warnings;
   close $fh;
+
+  ## Get ranges for PCA/UMAP to pass to graph tool....
   if( exists $res->{'pca'} ) {
     $res->{'ranges'}{'pca'} =  [ [ floor( $r{'p1'}[0]), ceil( $r{'p1'}[1]) ],
                                  [ floor( $r{'p2'}[0]), ceil( $r{'p2'}[1]) ] ];
@@ -165,7 +172,5 @@ sub parse {
     $res->{'umap'} = $res->{'umap'} == 3 ? [ $cols{'u1'},$cols{'u2'},$cols{'u3'} ] : [ $cols{'u1'},$cols{'u2'}];
   }
   $res->{'data'} = \@data;
-  foreach my $col (@{$res->{'columns'}}) {
-    $res->{'values'}{$col} = [ sort keys %{$v{$col}} ];
-  }
+  $res->{'values'}{$_} = [ sort keys %{$v{$col}} ] foreach @{$res->{'columns'}};
 }
